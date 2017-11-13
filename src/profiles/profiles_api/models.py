04ac56bd2270a73cd81a3e56c0213f7c4aa5fe71
JeffMinsungKim <ziznzkak@gmail.com>
2017-11-14 04:34:20 +0900

    Added UserProfileManager

--- src/profiles/profiles_api/models.py
+++ src/profiles/profiles_api/models.py
@@ -1,6 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager works with the custom user model."""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile object."""
+        if not email:
+            raise ValueError('Users must have an email address.')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details."""
+        user = self.creaete_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Represent a "user profile" inside our system."""
@@ -17,15 +45,12 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def get_full_name(self):
         """Used to get a users fulle name."""
-
         return self.name
 
     def get_short_name(self):
         """Used to get a users short name."""
-
         return self.name
 
     def __str__(self):
-        """Uses this method when it needs to convert the object to a string."""
-
+        """It needs to convert the object to a string."""
         return self.email
